0e4d02561a27693a55de460379236f838dd222ec
Corey Plotkin <corey.plotkin@gmail.com>
2021-09-01 12:34:50 -0500

    images with upload and show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :fix_orders
+  has_one_attached :photo
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_one_attached :photo
 
   # def uploaded_monkeys
   #   uploads = self.uploads
