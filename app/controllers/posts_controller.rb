ae8e64393c9c91a1bac7f2da8c4e9c79af302701
eduardo valenzuela <valenzuelagarza@gmail.com>
2021-09-02 17:52:38 -0500

    broken navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:description, :latitude, :longitude, :photo)
+    params.require(:post).permit(:description, :latitude, :longitude, :photo, :address)
   end
 
 end
