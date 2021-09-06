b6883050681434e71cf915bddbba6ec4e54884e3
eduardo valenzuela <valenzuelagarza@gmail.com>
2021-09-06 12:18:40 -0500

    cleaning stations index and fuctions

--- app/controllers/fix_orders_controller.rb
+++ app/controllers/fix_orders_controller.rb
@@ -1,7 +1,3 @@
 class FixOrdersController < ApplicationController
 
-  def index
-    @fix_orders = FixOrder.all
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   get "user_posts", to: "posts#user_posts", as: "user_posts"
 
-  resources :fix_orders, only: :index
+  resources :cleaning_stations, only: :index
 end
