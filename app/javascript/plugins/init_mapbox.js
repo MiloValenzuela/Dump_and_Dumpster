90614d01271e6102baaddd282ff363e8a4168cec
eduardo valenzuela <valenzuelagarza@gmail.com>
2021-09-10 11:05:23 -0500

    markers in map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,10 +15,12 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-      new mapboxgl.Marker()
+      let marker_dom = new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
-        .setPopup(popup)
-        .addTo(map);
+        .setPopup(popup);
+
+      marker_dom._element.dataset.id= marker.id ;
+      marker_dom.addTo(map) ;
     });
     fitMapToMarkers(map, markers);
   }
@@ -29,17 +31,6 @@ const fitMapToMarkers = (map, markers) => {
   markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
   map.addControl(new mapboxgl.NavigationControl());
-};
-
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
-
-    new mapboxgl.Marker()
-      .setLngLat([marker.lng, marker.lat])
-      .setPopup(popup) // add this
-      .addTo(map);
-  });
 };
 
 export { initMapbox };
